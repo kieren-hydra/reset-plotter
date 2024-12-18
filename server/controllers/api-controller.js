5a84a7fe7b8cb84daca18c3f5a4a4ec5840930cd
kieren-hydra <kieren@hydracreative.com>
2024-12-18 14:43:03 +0000

    created mock endpoint

--- server/controllers/api-controller.js
+++ server/controllers/api-controller.js
@@ -1,4 +1,121 @@
 
+
+
 export const test = (req, res) => {
-    res.json({ message: 'Hello World!' });
+
+    res.status(200).json({
+        success: true,
+        data:
+            {
+                companies: [
+                    {
+                        "name": "Company 1",
+                        "id": 1,
+                        "sites": [
+                            {
+                                "name": "Hospital",
+                                "id": 1234,
+                                "boundary": [
+                                    {
+                                        "lat": 53.34819631399739,
+                                        "lng": -1.4875218170248719
+                                    },
+                                    {
+                                        "lat": 53.34884258009266,
+                                        "lng": -1.484346129770862
+                                    },
+                                    {
+                                        "lat": 53.34261647597055,
+                                        "lng": -1.4872692055387575
+                                    },
+                                    {
+                                        "lat": 53.345358707241274,
+                                        "lng": -1.4906484603312755
+                                    }
+                                ],
+                                "terminals": [
+                                    {
+                                        "name": "Terminal 1",
+                                        "id": 12345,
+                                        "coordinates": {
+                                            "lat": 53.34819631399739,
+                                            "lng": -1.4875218170248719
+                                        }
+
+                                    },
+                                    {
+                                        "name": "Terminal 2",
+                                        "id": 12346,
+                                        "coordinates": {
+                                            "lat": 53.34884258009266,
+                                            "lng": -1.484346129770862
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                "name": "Factory",
+                                "id": 2345,
+                                "boundary": [
+                                    {
+                                        "lat": 53.341216015889806,
+                                        "lng": -1.4930792697193893
+                                    },
+                                    {
+                                        "lat": 53.340677365150114,
+                                        "lng": -1.4813508792926477
+                                    },
+                                    {
+                                        "lat": 53.33828567368466,
+                                        "lng": -1.4855730998542154
+                                    }
+                                ],
+                                "terminals": [
+                                    {
+                                        "name": "Terminal 3",
+                                        "id": 12347,
+                                        "coordinates": {
+                                            "lat": 53.341216015889806,
+                                            "lng": -1.4930792697193893
+                                        }
+                                    },
+                                    {
+                                        "name": "Terminal 4",
+                                        "id": 12348,
+                                        "coordinates": {
+                                            "lat": 53.340677365150114,
+                                            "lng": -1.4813508792926477
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "name": "Company 2",
+                        "id": 2,
+                        "sites": [
+                            {
+                                "name": "Warehouse",
+                                "id": 3456,
+                                "coordinates": []
+                            }
+                        ],
+                        "terminals": [
+                            {
+                                "name": "Terminal 5",
+                                "id": 12349,
+                                "coordinates": []
+                            },
+                            {
+                                "name": "Terminal 6",
+                                "id": 12350,
+                                "coordinates": []
+                            }
+                        ]
+                    }
+                ]
+            }
+    });
+
 };
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,21 @@
+// import {useQuery} from "@tanstack/react-query";
+// import {useEffect} from "react";
+// import {webService} from "../utils/api-utils.ts";
 
 const Dashboard = () => {
+
+    // const { data } = useQuery({
+    //     queryKey: ['locations'],
+    //     queryFn: () => webService.get('/test '),
+    // })
+    //
+    // useEffect(() => {
+    //     if(data) {
+    //         console.log("data",data)
+    //     }
+    // }, [data]);
+
+
     return (
         <div className="h-full w-[439px]">
             <h1>Dashboard</h1>
